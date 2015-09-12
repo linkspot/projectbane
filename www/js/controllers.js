53b8fc990344fe85ca5be65f201dea79946b2b5e
David Nguyen <davidhn1989@gmail.com>
2015-09-12 16:57:13 -0700

    implemented camera feature

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -21,8 +21,32 @@ angular.module('starter.controllers', [])
   $scope.chat = Chats.get($stateParams.chatId);
 })
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+
+.controller('CameraCtrl', ['$scope', '$cordovaCamera', '$ionicPlatform', function($scope, $cordovaCamera, $ionicPlatform) {
+
+    $ionicPlatform.ready(function() {
+      console.log("device is ready")
+        $scope.takePicture = function() {
+            var options = {
+                  quality: 50,
+                  destinationType: Camera.DestinationType.DATA_URL,
+                  sourceType: Camera.PictureSourceType.CAMERA,
+                  allowEdit: true,
+                  encodingType: Camera.EncodingType.JPEG,
+                  targetWidth: 100,
+                  targetHeight: 100,
+                  popoverOptions: CameraPopoverOptions,
+                  saveToPhotoAlbum: false
+            };
+
+            $cordovaCamera.getPicture(options).then(function(imageData) {
+                  var image = document.getElementById('myImage');
+                  image.src = "data:image/jpeg;base64," + imageData;
+            }, function(err) {
+              // error
+            });
+        }
+      
+    });    
+
+  }])
