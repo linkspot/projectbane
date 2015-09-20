e866bde6ed6fe2f523e80b6a85a81b4db4c0689d
angelali92 <92.angelali@gmail.com>
2015-09-19 19:50:02 -0700

    saving as file url instead of data

--- www/app/components/camera/camera-controller.js
+++ www/app/components/camera/camera-controller.js
@@ -2,53 +2,90 @@ angular.module('linkspot')
 
 .controller('CameraCtrl', ['$scope', '$cordovaCamera', '$ionicPlatform', 'CameraService', '$cordovaDevice', function($scope, $cordovaCamera, $ionicPlatform, CameraService, $cordovaDevice) {
     $scope.options = "hello";
+
     $ionicPlatform.ready(function() {
         console.log("device is ready");
-        var device = $cordovaDevice.getDevice();
-        $scope.options = device;
+        // var device = $cordovaDevice.getDevice();
+        // $scope.options = device;
         $scope.takePicture = function() {
             console.log("taking picture");
             var options = {
-                  quality: 50,
-                  destinationType: Camera.DestinationType.DATA_URL,
-                  sourceType: Camera.PictureSourceType.CAMERA,
-                  allowEdit: false,
-                  encodingType: Camera.EncodingType.PNG,
-                  targetWidth: 350,
-                  targetHeight: 200,
-                  popoverOptions: CameraPopoverOptions,
-                  saveToPhotoAlbum: true,
-                  correctOrientation: true
+                quality: 50,
+                destinationType: Camera.DestinationType.DATA_URL,
+                sourceType: Camera.PictureSourceType.CAMERA,
+                allowEdit: false,
+                encodingType: Camera.EncodingType.PNG,
+                targetWidth: 350,
+                targetHeight: 200,
+                popoverOptions: CameraPopoverOptions,
+                saveToPhotoAlbum: true,
+                correctOrientation: true
             };
 
             //$scope.options = options;
 
-            $cordovaCamera.getPicture(options).then(function(imageData) {
-              // Test
-              var image = document.getElementById('myImage');
-              image.src = "data:image/jpeg;base64," + imageData;
-              // TODO: Not clearing cache on phone.
-              CameraService.setProfile("data:image/jpeg;base64," + imageData);
+            // $cordovaCamera.getPicture(options).then(function(imageData) {
+            //     // Test
+            //     var image = document.getElementById('myImage');
+            //     image.src = "data:image/png;base64," + imageData;
 
-              // movePic(imageData);
+            //     CameraService.setProfile("data:image/png;base64," + imageData);
 
-            }, function(err) {
-              // error
-            });
+            // }, function(err) {
+            //     // error
+            // });
+
+            options["destinationType"] = Camera.DestinationType.FILE_URI;
+            $cordovaCamera.getPicture(options).then(function(imageURI) {
+                // Test
+                var image = document.getElementById('myImage');
+                image.src = imageURI;
+                $scope.options = imageURI;
+
+                CameraService.setProfile(imageURI);
+                $scope.movePic(imageURI);
+            })
         }
 
-        // function movePic(file){
-        //   window.resolveLocalFileSystemURI(file, resolveOnSuccess, resOnError);
-        // }
+        $scope.movePic = function(file){
+            $scope.options = "LocalFileSystem.PERSISTENT";
+            window.resolveLocalFileSystemURL(file, $scope.resolveOnSuccess, resOnError);
+        }
 
-        // function resolveOnSuccess(entry) {
-        //   var date = new Date();
-        //   var time = date.getTime();
+        $scope.resolveOnSuccess = function(entry) {
+            console.log("resolve on success");    
+            var date = new Date();
+            console.log(date);
+            var time = date.getTime();
 
-        //   var newFileName = time + ".png";
-        //   var myFolderApp = "ProjectBane";
-        // }
+            var newFileName = time + ".png";
+            var myFolderApp = "ProjectBane";
+            console.log(newFileName);
+
+            window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSys) {
+              // The folder is created if it doesn't exist
+              console.log("within request file system");
+              // TODO: Not working.
+              fileSys.root.getDirectory(myFolderApp, 
+                                      {created: true, exclusive: false},
+                                      function(directory) {
+                                            console.log("moveTo");
+                                            entry.moveTo(directory, newFileName, successMove, resOnError);
+                                      }, 
+                                      resOnError);
+            }, resOnError);
+        }
       
+        function successMove(entry) {
+          //Store imagepath in session for future use
+          // like to store it in database
+          //sessionStorage.setItem('imagepath', entry.fullPath);
+          console.log("moved successfully");
+        }
+
+        function resOnError(error) {
+          alert(error.code);
+        }
     });    
 
   }]);
