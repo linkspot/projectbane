e74639c1c1452b95b53ffaad950f8e3b9ba4f481
Angela Li <92.angelali@gmail.com>
2016-01-14 01:34:09 -0800

    beginnings of share contact

--- www/app/components/contacts/contacts-share-controller.js
+++ www/app/components/contacts/contacts-share-controller.js
@@ -1,16 +1,26 @@
 angular.module('linkspot')
 
-.controller('ContactsShareEmailCtrl', ['$scope', '$state', '$stateParams','$ionicSideMenuDelegate', 'Contacts', 'Email', 
-            function($scope, $state, $stateParams, $ionicSideMenuDelegate, Contacts, Email, Tags) {
+.controller('ContactsShareEmailCtrl', ['$scope', '$state', '$stateParams','$ionicSideMenuDelegate', 'Contacts', 'Email', 'Users',
+            function($scope, $state, $stateParams, $ionicSideMenuDelegate, Contacts, Email, Users) {
+
     $scope.$on('$ionicView.beforeEnter', function (event, viewData) {
-    	$scope.contact = Contacts.get($stateParams.contactId);
-    	$scope.title = "Share Contact";
+        $ionicSideMenuDelegate.toggleRight(false);
+        $scope.user = Users.get();
+
+        $scope.contact = Contacts.get($stateParams.contactId);
+        $scope.name = $scope.contact.firstName + " " + $scope.contact.lastName;
+        $scope.title = "Share Contact";
+
+        $scope.recipName = "";
+        $scope.recipEmail = "Hi";
     });
 
     // Send Email - can only populate email for app to use.
     $scope.sendEmail = function() {
-        var to = "angelgirl2272@gmail.com";
-        var from = "angelal4@uci.edu";
+        var to = $scope.recipEmail;
+        var from = $scope.user.email;
+        console.log(from);
+        console.log(to);
         var subject = "Hello - Testing from detail controller";
         var msg = "test test";
         Email.send(to, from, subject, msg).success(function() {
