095248c87bb290a3e55715ecdad60e18288ee20c
David Nguyen <davidhn1989@gmail.com>
2016-01-28 23:07:35 -0800

    updated notes to be a popup and a single line of text

--- www/app/components/contacts/controllers/contacts-detail-controller.js
+++ www/app/components/contacts/controllers/contacts-detail-controller.js
@@ -1,6 +1,6 @@
 angular.module('linkspot')
 
-.controller('ContactsDetailCtrl', ['$scope', '$state', '$stateParams', '$ionicHistory', '$ionicPopup','$ionicSideMenuDelegate', '$cordovaCamera', '$jrCrop', '$timeout', 'Contacts', 'Camera', 'Email', 'Tags', 
+.controller('ContactsDetailCtrl', ['$scope', '$state', '$stateParams', '$ionicHistory', '$ionicPopup','$ionicSideMenuDelegate', '$cordovaCamera', '$jrCrop', '$timeout', 'Contacts', 'Camera', 'Email', 'Tags',
             function($scope, $state, $stateParams, $ionicHistory, $ionicPopup, $ionicSideMenuDelegate, $cordovaCamera, $jrCrop, $timeout, Contacts, Camera, Email, Tags) {
     
     $scope.$on('$ionicView.beforeEnter', function (event, viewData) {
@@ -9,6 +9,9 @@ angular.module('linkspot')
         console.log($scope.contact);
         $scope.name = $scope.contact.firstName + " " + $scope.contact.lastName;
         $scope.title = $scope.name;
+
+        $scope.contactEdit = angular.copy(Contacts.get($stateParams.contactId));
+        $scope.contactUpdate = Contacts.get($stateParams.contactId);
 
         $scope.selectedTags = $scope.contact.tags;
         if ($scope.selectedTags == null)
@@ -49,6 +52,30 @@ angular.module('linkspot')
     $scope.toggleRightSideMenu = function() {
         $ionicSideMenuDelegate.toggleRight();
     };
+
+    $scope.updateContactNotes = function() {
+        $scope.contactUpdate.notes = $scope.contactEdit.notes;
+        $scope.contact.notes = $scope.contactEdit.notes;
+        Contacts.update($scope.contactUpdate);
+    };
+
+    $scope.showPopup = function() {
+      var myPopup = $ionicPopup.show({
+        template: '<input type="text" class="notes-popup" ng-model="contactEdit.notes">',
+        title: 'Enter Notes',
+        scope: $scope,
+        buttons: [
+          { text: 'Cancel' },
+          {
+            text: '<b>Save</b>',
+            type: 'button-positive',
+            onTap: function(e) {
+                $scope.updateContactNotes();
+            }
+          }
+        ]
+      });
+    }
 
 
 }]);
